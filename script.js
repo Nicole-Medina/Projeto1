997a401c58ef44103d5d4436ec5b5a72cad1a81c
Nicole Medina <nicoleagatam23@gmail.com>
2024-02-28 21:28:59 -0300

    first old commit

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
 const main = document.getElementById("main");
-const bloco = document.querySelector('.bloco');
-const logarButton = document.getElementById("lgr");
-const cadastrarButton = document.getElementById("cdstr");
-
-
-
-
-cadastrarButton.addEventListener('click', function() {
-    main.classList.add('animacao-deslizar_esq')
-    console.log("esquerda")
-});
-
-logarButton.addEventListener('click', function() {
-    main.classList.add('animacao-deslizar_dir')
-    console.log("direita")
-});
-
-
-
-/*
-
-
-
-
-
-const main = document.getElementById("main");
 const entrarButton = document.getElementById("login");
 const particButton = document.getElementById("registro");
 
@@ -37,4 +11,3 @@ entrarButton.addEventListener('click', function() {
     main.classList.remove("container.right-painel-active");
     console.log("Loguei")
 });
-*/
